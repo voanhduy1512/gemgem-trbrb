f5ef90322b5ea872e23c4213c0fe8843e568342e
Nick Sutterer <apotonick@gmail.com>
2015-09-21 16:55:49 +1000

    use new TRB api.

--- app/controllers/api/things_controller.rb
+++ app/controllers/api/things_controller.rb
@@ -1,26 +1,15 @@
 class Api::ThingsController < ApplicationController
   respond_to :json
-  # operation document_formats: :json
 
   def show
     respond Thing::Show
   end
 
   def create
-    # raise request.inspect
-    puts "@@@@@ #{params.inspect}"
+    # puts "@@@@@ #{params.inspect}"
     # op = Thing::Create.(params)
 
     # render json: op.to_json, location: "/op", status: :created
-
-    _params = params
-    _params[:thing] = params
-    respond Thing::Create, _params #, namespace: :api #, location: "/op/"
-  end
-
-  # TODO: Implement :namespace.
-  def respond(operation_class, params=self.params, respond_options = {}, &block)
-    res, op = operation!(operation_class, params) { operation_class.run(params) }
-    respond_with :api, op, respond_options
+    respond Thing::Create, namespace: [:api] #, location: "/op/"
   end
 end
