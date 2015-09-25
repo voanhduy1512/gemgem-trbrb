d76af903bdd89811538c4e653a0dfb22b5ed3f08
Nick Sutterer <apotonick@gmail.com>
2015-09-25 22:10:16 +1000

    cleaner tests. move this to trb or roar testing.

--- test/api/things_test.rb
+++ test/api/things_test.rb
@@ -8,29 +8,32 @@ class ApiThingsTest < MiniTest::Spec
     Rails.application
   end
 
+  def post(uri, data)
+    super(uri, data, "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT"=>"application/json")
+  end
+
+  def get(uri)
+    super(uri, nil, "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT"=>"application/json")
+  end
+
   it do
     id = Thing::Create.(thing: {name: "Rails"}).model.id
-    get "/api/things/#{id}", "", "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT"=>"application/json"
+    get "/api/things/#{id}"
     last_response.body.must_equal %{{"name":"Rails","authors":[],"id":#{id},"comments":[],"_links":{"self":{"href":"/api/things/#{id}"}}}}
   end
 
   it "post" do
-    data = {name: "Lotus"}
-    # request.env["HTTP_ACCEPT"] = "application/json"
-    post "/api/things/", data.to_json, "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT"=>"application/json"
+    post "/api/things/", {name: "Lotus"}.to_json
     id = Thing.last.id
 
     last_response.headers["Location"].must_equal "http://example.org/api/things/#{id}"
     assert last_response.created?
     last_response.body.must_equal %{{\"name\":\"Lotus\",\"authors\":[],\"id\":#{id},\"_links\":{\"self\":{\"href\":\"/api/things/#{id}\"}}}}
-
-    # assert_equal "http://example.org/redirected", last_request.url
-    # assert last_response.ok?
   end
 
   it "post allows adding authors" do
     data = {name: "Lotus", authors: [{email: "fred@trb.org"}]}
-    post "/api/things/", data.to_json, "CONTENT_TYPE" => "application/json", "HTTP_ACCEPT"=>"application/json"
+    post "/api/things/", data.to_json
 
     id = Thing.last.id
     author_id = Thing.last.users.first.id
