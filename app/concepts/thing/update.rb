d9d96f59affd0000d2130e659b9fd30fccf3711f
Nick Sutterer <apotonick@gmail.com>
2015-10-01 22:08:19 +1000

    T:Update::SignedIn now has a "proper" populator.

--- app/concepts/thing/update.rb
+++ app/concepts/thing/update.rb
@@ -9,7 +9,28 @@ class Thing < ActiveRecord::Base
         property :name, writeable: false
 
         # DISCUSS: should inherit: true be default?
-        collection :users, inherit: true, skip_if: :skip_user? do
+        # collection :users, inherit: true, skip_if: :skip_user? do
+        collection :users, inherit: true, populator: ->(fragment:, collection:, index:, **) {
+            puts "))) #{users.inspect}"
+            user = users.find { |u| u.id.to_s == fragment["id"].to_s }
+            puts "@@@@@ user: #{user.inspect}"
+            puts fragment.inspect
+            puts
+            if fragment["remove"].to_s == "1" and users.delete(user)
+              return Representable::Pipeline::Stop
+            end
+
+            return Representable::Pipeline::Stop if collection[index] # populate-if_empty
+            puts "...............#{collection[index]}"
+
+
+            # raise fragment.inspect
+            # raise user.inspect
+
+
+            users.insert index, User.new
+            } do
+          property :id
           property :email, skip_if: :skip_email?
 
           def skip_email?(fragment, options)
