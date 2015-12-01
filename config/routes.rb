baf160798b1b4167f1f253cb5485e2f2ed69fec1
Nick Sutterer <apotonick@gmail.com>
2015-12-01 18:12:30 +1100

    a reminder that rails is shit and i want controllers that are named Comment::Controller.

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :things
+      resources :things#, to: API::V1::Thing::Controller
       resources :users
       resources :comments
     end
